40206b017e41b06f1352792832896511cad2f538
Heta <hhmj28@gmail.com>
2025-03-10 01:17:44 +0200

    Added model

--- app.py
+++ app.py
@@ -1,42 +1,27 @@
-import os
-import numpy as np
-from PIL import Image
-import json
 from flask import Flask, request, jsonify, render_template
+from roboflow import Roboflow
+import os
+from dotenv import load_dotenv
 
-#Luo tarvittavat tiedostot/varmistaa niiden olemassaolon
 app = Flask(__name__)
 UPLOAD_FOLDER = 'uploads'
-VECTOR_FILE = 'vectors.json'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+load_dotenv()
+api_key = os.getenv('ROBOFLOW_API_KEY')
+
+# Roboflow API
+rf = Roboflow(api_key=api_key)
+project = rf.workspace().project("mushroom-xtwbh")
+model = project.version(1).model
+
+#toisen mallin käyttöönotto odottaa vielä lol
+#rf2 = Roboflow(api_key=API_KEY)
+#project2 = rf2.workspace().project("fungeye")
+#model2 = project2.version(2).model 
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-#vektoroi
-#silleen että ottaa vaan tietyt pikselit kuvasta --> katso netistä miten saa keskitettyä siihen
-#poista tausta kuvista jos mahdollista
-#katso yolov8 custom database 
-def image_to_vector(image_path):
-    with Image.open(image_path) as img:
-        img = img.resize((32, 32))
-        vector = np.array(img).flatten().tolist()
-    return vector
-
-def save_vector_to_file(file_name, vector):
-    data = {}
-    if os.path.exists(VECTOR_FILE):
-        with open(VECTOR_FILE, 'r') as f:
-            try:
-                data = json.load(f)
-            except json.JSONDecodeError:
-                data = {}
-    
-    data[file_name] = ','.join(map(str, vector))  # Tallennetaan vektorit merkkijonona yhteen riville
-    
-    with open(VECTOR_FILE, 'w') as f:
-        json.dump(data, f, indent=4)
-
-#käyttää html sivua
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -48,20 +33,11 @@ def upload_image():
     
     file = request.files['image']
     file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-    
-    # Varmistetaan, että tiedosto tallennetaan eri nimellä, jos samanniminen on jo olemassa
-    base, ext = os.path.splitext(file.filename)
-    counter = 1
-    while os.path.exists(file_path):
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], f"{base}_{counter}{ext}")
-        counter += 1
-    
     file.save(file_path)
-    
-    vector = image_to_vector(file_path)
-    save_vector_to_file(os.path.basename(file_path), vector)
-    
-    return jsonify({"vector": vector, "file_path": file_path})
+
+    prediction = model.predict(file_path).json()
+
+    return jsonify({"prediction": prediction})
 
 if __name__ == '__main__':
     app.run(debug=True)
