cc8d6fd578f6a584893860b40ff00d9aa1cd5868
Heta <hhmj28@gmail.com>
2025-04-07 17:01:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -116,4 +116,4 @@ def get_annotated_image(filename):
     return send_file(os.path.join(ANNOTATED_FOLDER, "annotated_" + filename), mimetype='image/jpeg')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=8080)
